8b905412c068ac62da52cc9d2ae3fe72afa64389
lvcq <lvcq617@aliyun.com>
2020-11-26 16:46:53 +0800

    pm2启动

--- build.sh
+++ build.sh
@@ -4,9 +4,14 @@ if ! command -v node-sass >/dev/null 2>&1; then
     npm install node-sass -g
 fi
 
+if ! command -v pm2 >/dev/null 2>&1;then
+    npm install -g pm2
+fi
+
 #build
 echo $(npm install)
 
 echo $(npm run build:theme)
 echo $(npm run build:ssr)
-node dist/zlyt-web/server/main.js
+pm2 start ecosystem.config.js
+pm2 plus
---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    name: 'zlyt-web',
+    script: 'dist/zlyt-web/server/main.js',
+    instances  : 4,
+    watch: true
+  }],
+
+  deploy : {
+    production : {
+      user : 'SSH_USERNAME',
+      host : 'SSH_HOSTMACHINE',
+      ref  : 'origin/master',
+      repo : 'GIT_REPOSITORY',
+      path : 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};

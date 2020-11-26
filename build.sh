985e2cce2b7b7836f0e17fabbd33239d812e1937
lvcq <lvcq617@aliyun.com>
2020-11-26 16:48:39 +0800

    修改启动

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ if ! command -v node-sass >/dev/null 2>&1; then
 fi
 
 if ! command -v pm2 >/dev/null 2>&1;then
+    echo "安装PM2"
     npm install -g pm2
 fi
 
+echo $(pm2 stop ecosystem.config.js)
 #build
 echo $(npm install)
 
 echo $(npm run build:theme)
 echo $(npm run build:ssr)
-pm2 start ecosystem.config.js
-pm2 plus
+echo $(pm2 start ecosystem.config.js)
+echo $(pm2 plus)
